e3c82c68a32e92daca4e03e65087e5c14221778a
Yunhwan Park <pyh8618@gmail.com>
2020-09-28 22:12:33 +0900

    200928 Retry 스도쿠

--- CodePlus_Practice/BruteForce/스도쿠.py
+++ CodePlus_Practice/BruteForce/스도쿠.py
@@ -47,3 +47,51 @@ def DFS(index):
 sudoku = [list(map(int, input().split())) for _ in range(9)]
 zeros = [(i, j) for i in range(9) for j in range(9) if sudoku[i][j] == 0]
 DFS(0)
+
+#my solve
+import sys
+
+
+def solve(index):
+    if index == len(zero):
+        for com in arr:
+            for c in com:
+                print(c, end=" ")
+            print()
+        sys.exit(0)
+    sel = zero[index]
+    x = sel[0]; y = sel[1]
+    # 가로 세로
+
+    for val in range(1, 10):
+        flag = True
+        if val in arr[x]:
+            continue
+        for k in range(9):
+            if arr[k][y] == val:
+                flag = False
+                break
+        if flag is False:
+            continue
+
+        for i in range(3):
+            for j in range(3):
+                start_x = int((x//3)*3); start_y = int((y//3)*3)
+                if val == arr[start_x + i][start_y + j]:
+                    flag = False
+                    continue
+        if flag is True:
+            arr[x][y] = val
+            solve(index+1)
+            arr[x][y] = 0
+
+
+arr = [list(map(int,sys.stdin.readline().rstrip().split())) for _ in range(9)]
+zero = []
+for i in range(9):
+    for j in range(9):
+        if arr[i][j] == 0:
+            zero.append((i, j))
+solve(0)
+
+
