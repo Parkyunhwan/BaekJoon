7a982c7708a432a05b040ba8ff59194c7afbd3ed
Yunhwan Park <pyh8618@gmail.com>
2020-10-04 03:02:15 +0900

    201004 Mod DSLR err

--- CodePlus_Practice/BFS/DSLR.py
+++ CodePlus_Practice/BFS/DSLR.py
@@ -3,42 +3,59 @@ from collections import deque
 t = int(input())
 
 
-def D(v, path):
-    return (2 * v) % 10000, path.append("D")
+def D(v):
+    return (2 * v) % 10000
 
 
-def S(v,path):
-    return 9999 if v == 0 else v - 1, path.append("S")
+def S(v):
+    return 9999 if v == 0 else v - 1
 
 
-def L(v,path):
-    v = str(v)
-    tmp = v[1:] + v[:1]
-    return tmp, path.append("L")
+def L(v):
+    le = str(v)
+    length = len(le)
+    if length != 4:
+        v = v*10
+    else:
+        v, d = divmod(v, 10**(length-1))
+        v += d*10
+    return v
 
 
-def R(v,path):
-    v = str(v)
-    tmp = v[-1] + v[:-1]
-    return tmp, path.append("R")
+def R(v):
+    re = str(v)
+    length = len(re)
+    if length == 1:
+        v = v*1000
+    else:
+        v, d = divmod(v, 10)
+        v += (d*1000)
+    return v
 
 
 for _ in range(t):
     a, b = list(map(int, input().split()))
+    check = [False] * 10000  #
     q = deque()
-    q.append((a, list()))
+    q.append((a, ""))
     while q:
         val, path = q.popleft()
         if val == b:
-            print("".join(path))
+            print(path)
             break
         for i in range(4):
             if i == 0:
-                val, path = D(val, path)
+                t = D(val)
+                tmp = path[:] + "D"  #
             elif i == 1:
-                val, path = S(val)
+                t = S(val)
+                tmp = path[:] + "S"
             elif i == 2:
-                val, path = int(L(val))
+                t = L(val)
+                tmp = path[:] + "L"
             else:
-                val, path = int(R(val))
-        q.append((val, path))
+                t = R(val)
+                tmp = path[:] + "R"
+            if not check[t]:
+                check[t] = True
+                q.append((t, tmp))
