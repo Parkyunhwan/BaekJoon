4b06e4c0da97c0e77ca354f3a74d1cede055ebd4
Yunhwan Park <pyh8618@gmail.com>
2020-10-07 16:23:06 +0900

    201007 DSLR 재풀이 - BFS문제 숫자의 자릿수 바꾸기

--- CodePlus_Practice/BFS/DSLR.py
+++ CodePlus_Practice/BFS/DSLR.py
@@ -1,61 +1,54 @@
+#  전형적인 BFS 문제이며 특별한 것은 딱하나 있었다. -> 어떻게 LEFT와 RIGHT를 네자리수가 아닐때에도 적절히 구현할 것인가..?
+#  --> L : 첫번째 자릿수는 나누기 1000을 통해 나머지 세자리는 나머지 1000을 통해 구할 수 있다.
+#  --> R : 첫번째 부터 세번째 자릿수까지는 나누기 10을 통해 네번째 자리는 나누기 10을 통해 얻을 수 있다.
+#  명령어 D : Y = (X*2) % 10000
+#  명령어 S : Y = X-1 (단, Y < 0일 경우, Y = 9999)
+#  명령어 L : Y = (X%1000)*10 + X/1000
+#  명령어 R : Y = X/10 + (X%10)*1000
+#출처: https://rebas.kr/764 [PROJECT REBAS]
+
+#  my code
 from collections import deque
 
 t = int(input())
 
 
-def D(v):
-    return (2 * v) % 10000
+def move(index, _a, pa):
+    if index == 0:  # D
+        return _a * 2 % 10000, pa + "D"
+    elif index == 1:  # S
+        return 9999 if _a == 0 else _a - 1, pa + "S"
+    elif index == 2:  # L
+        # 1234 -> 234 1, 0012 -> 0120
+        first = _a // 1000
+        second = _a % 1000
+        _a = (second * 10) + first
+        return _a, pa + "L"
+    else:  # R
+        # 1234 -> 4 123, 0012 -> 2001
+        first = _a // 10
+        second = _a % 10
+        _a = (second * 1000) + first
+        return _a, pa + "R"
 
 
-def S(v):
-    return 9999 if v == 0 else v - 1
-
-
-def L(v):
-    le = str(v)
-    length = len(le)
-    if length != 4:
-        v = v*10
-    else:
-        v, d = divmod(v, 10**(length-1))
-        v += d*10
-    return v
-
-
-def R(v):
-    re = str(v)
-    length = len(re)
-    if length == 1:
-        v = v*1000
-    else:
-        v, d = divmod(v, 10)
-        v += (d*1000)
-    return v
-
-
-for _ in range(t):
-    a, b = list(map(int, input().split()))
-    check = [False] * 10000  #
+def BFS():
     q = deque()
     q.append((a, ""))
     while q:
-        val, path = q.popleft()
-        if val == b:
-            print(path)
-            break
+        sa, path = q.popleft()
+        if sa == b:
+            return path
         for i in range(4):
-            if i == 0:
-                t = D(val)
-                tmp = path[:] + "D"  #
-            elif i == 1:
-                t = S(val)
-                tmp = path[:] + "S"
-            elif i == 2:
-                t = L(val)
-                tmp = path[:] + "L"
-            else:
-                t = R(val)
-                tmp = path[:] + "R"
-            if not check[t]:
-                check[t] = True
-                q.append((t, tmp))
+            na, npath = move(i, sa, path)
+            if not check[na]:
+                check[na] = True
+                q.append((na, npath))
+
+
+for _ in range(t):
+    check = [False] * 10001
+    a, b = map(int, input().split())
+    print(BFS())
+
+
