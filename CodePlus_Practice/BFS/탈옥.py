6db1c583833ad25cc44481aa836a1a4225814ba7
Yunhwan Park <pyh8618@gmail.com>
2020-10-09 22:06:11 +0900

    201009 Add 탈옥 - 최솟값을 구하기 위해선 작은 값으로 먼저 방문

--- CodePlus_Practice/BFS/탈옥.py
+++ CodePlus_Practice/BFS/탈옥.py
@@ -1,63 +1,63 @@
-#  죄수가 두 명인 문제
-#  기존과는 다른 방식으로 생각해야함
-#  두 명이 같이 움직이는 것을 어떻게 표현할까? -> 일단 rebas님은 하나씩 BFS를 수행함
-#  한 명이 탈출했을 때 남아 있는 다른 한명은 어떻게 진행할 것인가? -> 따로 진행
+#'.' 과 '#' 이 둘 다 front 혹은 back으로 들어가면
+# 먼저 dist가 갱신된 경우 올바른 값을 출력하지 못할 수 있다.
+# front부터 빼내고 있기 때문에 '.'이 front로 들어가야 최솟값을 찾을 수 있음
+# 문을 여는 비용이 적은 녀석들을 먼저 탐색할 수 있도록 큐 대신 Deque를 사용해서 구현
 from sys import stdin
 from collections import deque
+t = int(input())
 
-n = int(input())
+dx = (-1, 1, 0, 0)
+dy = (0, 0, -1, 1)
 
-dx = (0, 0, -1, 1)
-dy = (-1, 1, 0, 0)
-
-
-def BFS(x, y):
+def BFS(sx, sy):
+    dist = [[-1]*(w+2) for _ in range(h+2)]
     q = deque()
-    dist = [[-1] * (w + 2) for _ in range(h + 2)]
-    q.append((x, y))
-    dist[x][y] = 0
+    dist[sx][sy] = 0
+    q.append((sx, sy))
     while q:
         tx, ty = q.popleft()
         for k in range(4):
-            nx, ny = tx + dx[k], ty + dy[k]
-            if nx < 0 or nx > h + 1 or ny < 0 or ny > w + 1:
+            nx, ny = tx+dx[k], ty+dy[k]
+            if nx < 0 or nx >= h+2 or ny < 0 or ny >= w+2:
                 continue
-            if dist[nx][ny] >= 0 or arr[nx][ny] == '*':
+            if arr[nx][ny] == '*' or dist[nx][ny] != -1:
                 continue
-            if arr[nx][ny] == '.':
-                dist[nx][ny] = dist[tx][ty]
-                q.append((nx, ny))
             elif arr[nx][ny] == '#':
                 dist[nx][ny] = dist[tx][ty] + 1
                 q.append((nx, ny))
+            else:
+                dist[nx][ny] = dist[tx][ty]
+                q.appendleft((nx, ny))
     return dist
 
 
-for _ in range(n):
+for _ in range(t):
     h, w = map(int, input().split())
-    arr = ['.'*(w + 2)]
+    arr = ['.'*(w+2)]
     for _ in range(h):
-        arr.append(list('.' + stdin.readline().strip() + '.'))
-    arr.append(list('.' * (w + 2)))
-    prisoner = deque()
-    pris = 0
-    for i in range(h + 2):
-        for j in range(w + 2):
+        arr.append('.'+stdin.readline().strip()+'.')
+    arr.append('.'*(w+2))
+    #  시작 위치 찾기
+    start = []
+    for i in range(h+2):
+        for j in range(w+2):
             if arr[i][j] == '$':
-                arr[i][j] = '.'
-                prisoner.append((i, j))
-    fx, fy = prisoner.popleft()
-    df = BFS(fx, fy)
-    sx, sy = prisoner.popleft()
-    ds = BFS(sx, sy)
+                # arr[i][j] = '.'  # 예외를 만들지 않기 위해 '.'으로 바꿔주는 것 중요
+                start.append((i, j))
+
+    move_arr = []
+    for x, y in start:
+        move_arr.append(BFS(x, y))
     dz = BFS(0, 0)
-    ans = 1e9
+    mn = 987654321
     for i in range(h+2):
         for j in range(w+2):
             if arr[i][j] == '*':
                 continue
-            k = df[i][j] + ds[i][j] + dz[i][j]
+            sm = dz[i][j]
+            for t in range(len(move_arr)):
+                sm += move_arr[t][i][j]
             if arr[i][j] == '#':
-                k -= 2
-            ans = min(ans, k)
-    print(ans)
+                sm -= len(move_arr)
+            mn = min(mn, sm)
+    print(mn)
