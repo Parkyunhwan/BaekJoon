695ce8761a5c2b6b02a0c755da7813d9d8380c32
Yunhwan Park <pyh8618@gmail.com>
2020-10-09 21:59:26 +0900

    201009 Mod 숨바꼭질 Comment - BFS 방문체크 push 전 후 차이 구분

--- CodePlus_Practice/BFS/숨바꼭질2.py
+++ CodePlus_Practice/BFS/숨바꼭질2.py
@@ -1,4 +1,3 @@
-#  https://jaimemin.tistory.com/582
 #  방문 검사를 앞에 하는 것과 뒤에 하는 것의 차이는 무엇인가? -> 앞에서 하면 동시에 들어온 값들이 체크되지 않는다.
 #  따라서, 동시에 들어온 중복 값들을 체크하려면 pop후에 방문 체크를 해야한다.
 
@@ -12,7 +11,7 @@ num = 0
 while q:
 
     su, count = q.popleft()
-    check[su] = True
+    check[su] = True  #  pop 이후에 체크함으로서 동시에 들어온 것 까지는 인정할 수 있다.
     if su == k:
         print(count)
         num = 1
@@ -27,7 +26,8 @@ while q:
 
         exit(0)
     if su - 1 >= 0 and not check[su-1]:
-        #check[su - 1] = True # 틀린 코드 작품
+        #check[su - 1] = True # 틀린 코드 ( 맨 첫번째 들어올 때 체크 하기 때문에 )
+        #                              동시에 들어온 애들의 갯수를 체크할 수 없다.
         # if su-1 == k:
         #     check[su - 1] = False
         #     num += 1
