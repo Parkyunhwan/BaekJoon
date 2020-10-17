cb64306ce67d8950ea75c2f4bef13811fecadb23
Yunhwan Park <pyh8618@gmail.com>
2020-10-17 21:52:08 +0900

    201017 Retry 구슬탈출2 - 구현력

--- CodePlus_Practice/BruteForce/구슬탈출2.py
+++ CodePlus_Practice/BruteForce/구슬탈출2.py
@@ -56,3 +56,83 @@ def bfs():
                 q.appendleft(((nbx, nby), (nrx, nry), d+1))
     print(-1)
 bfs()
+
+
+# 3회차 코드
+
+from collections import deque
+
+n, m = map(int, input().split())
+arr = [list(input()) for _ in range(n)]
+red, blue = [], []
+check = [[[[False]*m for _ in range(n)] for _ in range(m)] for _ in range(n)]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+
+def check_wall(rx, ry, bx, by, tx, ty):
+    count_r = 0
+    count_b = 0
+    flag = False
+    while arr[rx+tx][ry+ty] != '#':
+        rx += tx
+        ry += ty
+        count_r += 1
+        if arr[rx][ry] == 'O':
+            flag = True
+            break
+
+    while arr[bx+tx][by+ty] != '#':
+        bx += tx
+        by += ty
+        count_b += 1
+        if arr[bx][by] == 'O':
+            flag = True
+            break
+
+    if not flag and rx == bx and ry == by:
+        if count_r > count_b:
+            rx -= tx
+            ry -= ty
+        else:
+            bx -= tx
+            by -= ty
+
+    return rx, ry, bx, by
+
+
+def move(dir, rx, ry, bx, by):
+    return check_wall(rx, ry, bx, by, dx[dir], dy[dir])
+
+
+def BFS():
+    q = deque()
+    q.append((red, blue, 0))
+    while q:
+        r, b, count = q.popleft()
+        if count >= 10:
+            break
+        rx, ry = r[0], r[1]
+        bx, by = b[0], b[1]
+
+        for i in range(4):
+            nrx, nry, nbx, nby = move(i, rx, ry, bx, by)
+            if arr[nbx][nby] == 'O':
+                continue
+            elif arr[nrx][nry] == 'O':
+                return count + 1
+            if not check[nrx][nry][nbx][nby]:
+                check[nrx][nry][nbx][nby] = True
+                q.append(([nrx, nry], [nbx, nby], count + 1))
+    return -1
+
+
+for i in range(n):
+    for j in range(m):
+        if arr[i][j] == 'R':
+            arr[i][j] = '.'
+            red = [i, j]
+        elif arr[i][j] == 'B':
+            arr[i][j] = '.'
+            blue = [i, j]
+print(BFS())
