b0cedb281476900e9e834e7afb931b842cfdc372
Yunhwan Park <pyh8618@gmail.com>
2020-10-20 15:02:56 +0900

    201020 Add Retry Comment

--- 이것이_코딩테스트다_Book/알고리즘_유형별_기출문제/그리디문제/모험가길드.py
+++ 이것이_코딩테스트다_Book/알고리즘_유형별_기출문제/그리디문제/모험가길드.py
@@ -1,5 +1,17 @@
+# 2회차 => O (더 안해도 됨)
+n = int(input())
+arr = list(map(int, input().split()))
+arr.sort()
+result = 0
+count = 0
+for i in range(len(arr)):
+    val = arr[i]
+    count += 1
+    if val == count:
+        result += 1
+        count = 0
+print(result)
 # 1회차 => 35분
-# 2회차 =>
 n = int(input())
 arr = list(map(int, input().split()))
 check = 0
