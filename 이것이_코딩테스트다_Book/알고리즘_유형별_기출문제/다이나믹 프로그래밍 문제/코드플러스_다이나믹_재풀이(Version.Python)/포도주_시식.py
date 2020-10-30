e8235d75099351dc50c905fe97396f29d06c644f
Yunhwan Park <pyh8618@gmail.com>
2020-10-30 23:56:53 +0900

    201030 Add 포도주시식 retry

--- 이것이_코딩테스트다_Book/알고리즘_유형별_기출문제/다이나믹 프로그래밍 문제/코드플러스_다이나믹_재풀이(Version.Python)/포도주_시식.py
+++ 이것이_코딩테스트다_Book/알고리즘_유형별_기출문제/다이나믹 프로그래밍 문제/코드플러스_다이나믹_재풀이(Version.Python)/포도주_시식.py
@@ -9,3 +9,17 @@ for i in range(3, n+1):
     d[i] = max(d[i], d[i-3]+arr[i-1]+arr[i])
 
 print(d[n])
+
+
+# 2회차 실패 -> 현재 자신을 선택안하는 경우를 생각하지 못했다. 너무 어려운 생각인것같다.
+n = int(input())
+arr = [0] + [int(input()) for _ in range(n)]
+d = [0]*(n+2)
+d[1] = arr[1]
+d[2] = arr[1] + arr[2]
+
+for i in range(3, n+1):
+    d[i] = d[i-1]
+    d[i] = max(arr[i] + d[i-2], arr[i] + arr[i-1] + d[i-3])
+
+print(d[n])
