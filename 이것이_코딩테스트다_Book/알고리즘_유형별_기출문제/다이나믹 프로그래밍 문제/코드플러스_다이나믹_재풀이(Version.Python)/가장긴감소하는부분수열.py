b7766ec333e066025fde8269df04432d440125d1
Yunhwan Park <pyh8618@gmail.com>
2020-11-07 21:39:12 +0900

    201107 가장긴감소하는부분수열 mod comment

--- 이것이_코딩테스트다_Book/알고리즘_유형별_기출문제/다이나믹 프로그래밍 문제/코드플러스_다이나믹_재풀이(Version.Python)/가장긴감소하는부분수열.py
+++ 이것이_코딩테스트다_Book/알고리즘_유형별_기출문제/다이나믹 프로그래밍 문제/코드플러스_다이나믹_재풀이(Version.Python)/가장긴감소하는부분수열.py
@@ -1,6 +1,6 @@
 n = int(input())
 lst = list(map(int, input().split())) 
-dp = [1 for i in range(n)]
+dp = [1 for i in range(n)] # 자기 자신은 기본적으로 포함하고 있으므로 초깃값을 1로 설정한다.
 for i in range(n):
     for j in range(i): 
         if lst[i] < lst[j]: 
