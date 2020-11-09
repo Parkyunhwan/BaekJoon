7dc5adbe875bf988c610e106a5f7f440ad1dd811
Yunhwan Park <pyh8618@gmail.com>
2020-11-09 23:53:48 +0900

    201109 Add 연속합 Retry

--- 이것이_코딩테스트다_Book/알고리즘_유형별_기출문제/다이나믹 프로그래밍 문제/코드플러스_다이나믹_재풀이(Version.Python)/연속합.py
+++ 이것이_코딩테스트다_Book/알고리즘_유형별_기출문제/다이나믹 프로그래밍 문제/코드플러스_다이나믹_재풀이(Version.Python)/연속합.py
@@ -1,10 +1,24 @@
+# -1001 을 하거나 가장 작은수를 구하거나..
+# 숫자가 연속되므로 구하기 쉽다.
 n = int(input())
-num_list = list(map(int, input().split()))
-temp = [0 for _ in range(n)]
-result = -1001
+arr = [0] + list(map(int, input().split()))
+mn = min(arr)
+d = [mn] * (n+1)
 
-for i in range(n):
-    temp[i] = max(temp[i - 1] + num_list[i], num_list[i])
-    result = max(result, temp[i])
+for i in range(1, n+1):
+        d[i] = max(arr[i], arr[i] + d[i-1])
 
-print(result)
+print(max(d))
+
+
+# n = int(input())
+# num_list = list(map(int, input().split()))
+# temp = [0 for _ in range(n)]
+# result = -1001
+#
+# for i in range(n):
+#     temp[i] = max(temp[i - 1] + num_list[i], num_list[i])
+#     result = max(result, temp[i])
+#
+# print(result)
+
