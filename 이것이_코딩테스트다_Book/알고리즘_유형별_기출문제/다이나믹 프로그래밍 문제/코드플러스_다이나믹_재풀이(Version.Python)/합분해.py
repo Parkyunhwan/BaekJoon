faf6371c056034a0b9ada396b1212a37c682140f
박윤환 <pyh8618@gmail.com>
2020-11-16 01:14:00 +0900

    201115 Add 합분해 Comment and version2(3 iteration)

--- 이것이_코딩테스트다_Book/알고리즘_유형별_기출문제/다이나믹 프로그래밍 문제/코드플러스_다이나믹_재풀이(Version.Python)/합분해.py
+++ 이것이_코딩테스트다_Book/알고리즘_유형별_기출문제/다이나믹 프로그래밍 문제/코드플러스_다이나믹_재풀이(Version.Python)/합분해.py
@@ -1,8 +1,23 @@
-N, K = map(int, input().split())
+# N, K = map(int, input().split())
+# mod = 1000000000
+# table = [1]
+# table += [0] * N
+# for _ in range(1, K+1):
+#     for idx in range(1, N+1):
+#         table[idx] = (table[idx] + table[idx-1])%mod
+# print(table[N])
+
+n, k = map(int, input().split())
 mod = 1000000000
-table = [1]
-table += [0] * N
-for _ in range(1, K+1):
-    for idx in range(1, N+1):
-        table[idx] = (table[idx] + table[idx-1])%mod
-print(table[N])
+dp = [[0] * (n+1) for _ in range(n+1)]
+
+for i in range(n+1):
+    dp[1][i] = 1
+
+for i in range(1, n+1):
+    for j in range(n+1):
+        for l in range(n+1): # 0의 경우도 포함해야한다.
+            dp[i][j] += dp[i-1][j-l]
+            dp[i][j] %= mod
+
+print(dp[k][n])
