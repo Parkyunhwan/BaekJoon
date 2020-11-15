c266a7f23f2f360e9eff012af97b26cc024ed955
박윤환 <pyh8618@gmail.com>
2020-11-15 23:35:47 +0900

    201115 Add 제곱수의합 Comment

--- 이것이_코딩테스트다_Book/알고리즘_유형별_기출문제/다이나믹 프로그래밍 문제/코드플러스_다이나믹_재풀이(Version.Python)/제곱수의합.py
+++ 이것이_코딩테스트다_Book/알고리즘_유형별_기출문제/다이나믹 프로그래밍 문제/코드플러스_다이나믹_재풀이(Version.Python)/제곱수의합.py
@@ -1,8 +1,19 @@
-inputNum = int(input())
-nc = [0] * (inputNum+1)
-for i in range(1, inputNum+1):
-    nc[i] = i for j in range(1, i):
-        if (j * j) > i:
-            break
-        nc[i] = min(nc[i], nc[i - j * j] + 1)
-print(nc[inputNum])
+# inputNum = int(input())
+# nc = [0] * (inputNum+1)
+# for i in range(1, inputNum+1):
+#     nc[i] = i
+#     for j in range(1, i):
+#         if (j * j) > i:
+#             break
+#         nc[i] = min(nc[i], nc[i - j * j] + 1)
+# print(nc[inputNum])
+if __name__ == '__main__':
+    n = int(input())
+    d = [0] * (n + 1)
+    for i in range(1, n + 1):
+        d[i] = i
+        for j in range(1, n + 1):
+            if j * j > i:
+                break
+            d[i] = min(d[i], d[i - (j * j)] + 1)
+    print(d[n])
