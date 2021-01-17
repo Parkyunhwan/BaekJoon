d5340442108bbd63ea989e496cdfc704155d390a
yunhwan <pyh8618@gmail.com>
2021-01-18 01:47:40 +0900

    210118 Add retry 가장 긴 감소하는 부분 수열

---
+++ DP/가장 긴 감소하는 부분 수열.py
@@ -0,0 +1,14 @@
+import sys
+n = int(input())
+arr = list(map(int, input().split()))
+dp = [0] * n
+mx = 1
+
+for i in range(len(arr)):
+    dp[i] = 1
+    for j in range(i):
+        if arr[j] > arr[i] and dp[j] + 1 > dp[i]:
+            dp[i] = dp[j] + 1
+            mx = max(mx, dp[i])
+
+print(mx)
