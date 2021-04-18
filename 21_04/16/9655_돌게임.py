0ed9f18d403b69572e76ca7cb162be1bb263ab5a
yunhwan <pyh8618@gmail.com>
2021-04-18 23:53:45 +0900

    210418 돌게임 풀이

--- 21_04/16/9655_돌게임.py
+++ 21_04/16/9655_돌게임.py
@@ -2,8 +2,25 @@
     https://www.acmicpc.net/workbook/view/81
     코딩테스트 기출 (돌게임)
 '''
-N = int(input())
-if N % 2 == 0:
-    print("CY")
-else:
+# N = int(input())
+# if N % 2 == 0:
+#     print("CY")
+# else:
+#     print("SK")
+
+# 최소한의 게임
+
+n = int(input())
+dp = [0] * (n + 1)
+
+dp[1] = 1
+dp[2] = 2
+dp[3] = 1
+
+for i in range(4, n + 1):
+    dp[i] = min(dp[i - 3] + 1, dp[i - 1] + 1)
+
+if dp[n] % 2:
     print("SK")
+else:
+    print("CY")
