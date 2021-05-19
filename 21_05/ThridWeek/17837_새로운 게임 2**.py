137175613edf5af9dc2d77708277b61728a1a724
yunhwan <pyh8618@gmail.com>
2021-05-19 22:49:52 +0900

    210519 Mod 새로운 게임 2

--- 21_05/ThridWeek/17837_새로운 게임 2**.py
+++ 21_05/ThridWeek/17837_새로운 게임 2**.py
@@ -1,100 +1,66 @@
+'''
+    3시간 동안 코드 오류를 찾느라 헤멨다.
+
+    문제의 조건을 잘못읽어서 답이 나오지 않았따..
+    if len(stack_arr[nx][ny]) >= 4:
+    조건이 4보다 크다면인데 K보다 크다면으로 착각함..
+
+'''
+
 N, K = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
 
 stack_arr = [[[] for _ in range(N)] for _ in range(N)]
-horse = []
+horse = [[] for _ in range(K)]
 
 for i in range(K):
     x, y, dir = map(int, input().split())
-    horse.append((x - 1, y - 1, dir - 1))
+    horse[i] = [x - 1, y - 1, dir - 1]
     stack_arr[x - 1][y - 1].append(i)
 
 dx = [0, 0, -1, 1]
 dy = [1, -1, 0, 0]
 flag = False
+
 time = 1
-
-print(horse)
-for i in range(N):
-    for j in range(N):
-        print(stack_arr[i][j], end=" ")
-    print()
-
-
-while time <= 9:
-    print()
-    print("time : " + str(time))
-    for i in range(K):
-        x, y, curr_dir = horse[i]
-        print(i)
-
-        for a in range(N):
-            for b in range(N):
-                print(stack_arr[a][b], end=" ")
-            print()
-
-        tmp = []
-        tmp2 = []
-        for idx, val in enumerate(stack_arr[x][y]):
-            print(idx, val, i)
-            if val == i:
-                print("index value " + str(val))
-                tmp = stack_arr[x][y][idx:]
-                tmp2 = stack_arr[x][y][:idx]
+while time <= 1000:
+    for hi in range(K):
+        x, y, curr_dir = horse[hi]
 
         nx, ny = x + dx[curr_dir], y + dy[curr_dir]
         if nx < 0 or ny < 0 or nx >= N or ny >= N or arr[nx][ny] == 2:# blue
-            if curr_dir == 0:
-                curr_dir = 1
-            elif curr_dir == 1:
-                curr_dir = 0
-            elif curr_dir == 2:
-                curr_dir = 3
-            elif curr_dir == 3:
-                curr_dir = 2
-
-            nx, ny = x + dx[curr_dir], y + dy[curr_dir]
+            nhd = curr_dir-1 if curr_dir%2 else curr_dir+1
+            horse[hi][2] = nhd
+            nx, ny = x + dx[nhd], y + dy[nhd]
             if nx < 0 or ny < 0 or nx >= N or ny >= N or arr[nx][ny] == 2:  # blue
-                for index in tmp:
-                    if index == i:
-                        horse[index] = x, y, curr_dir
-                    else:
-                        horse[index] = x, y, horse[index][2]
                 continue
 
-        print(x, y, stack_arr[x][y])
+        temp = []
+        for i, key in enumerate(stack_arr[x][y]):
+            if key == hi:
+                # 지금 말보다 위에 있는것들을 temp에 넣어서 이동준비
+                temp = (stack_arr[x][y][i:])
+                # 지금 말보다 아래있는것들만 남김
+                stack_arr[x][y] = stack_arr[x][y][:i]
+                break
 
-        if arr[nx][ny] == 0: # white
-            for index in tmp:
-                horse[index] = nx, ny, horse[index][2]
-            stack_arr[nx][ny].extend(tmp)
-            stack_arr[x][y] = tmp2[:]
-        elif arr[nx][ny] == 1: # red
-            for index in tmp:
-                horse[index] = nx, ny, horse[index][2]
-            stack_arr[nx][ny].extend(reversed(tmp))
-            stack_arr[x][y] = tmp2[:]
+        # 다음 위치가 빨간색이면, 순서 뒤집음
+        if arr[nx][ny] == 1:
+            temp = reversed(temp)
 
+        # 다음 위치에 넣고, 위치 정보를 갱신함 방향은 그대로이므로 갱신X
+        for i in temp:
+            stack_arr[nx][ny].append(i)
+            horse[i][:2] = [nx, ny]
 
-        print(arr[nx][ny])
-
-        print(nx, ny, stack_arr[nx][ny])
-        print()
-
-        for a in range(N):
-            for b in range(N):
-                print(stack_arr[a][b], end=" ")
-            print()
-        if len(stack_arr[nx][ny]) == N:
+        if len(stack_arr[nx][ny]) >= 4:
             flag = True
             break
-
-
     if flag:
         break
     time += 1
+
 if time > 1000:
     print(-1)
 else:
-    print("return")
     print(time)
